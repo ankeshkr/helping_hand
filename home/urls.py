78804d2ca66d7ddc853d41c87f0966c38ea9612d
ankesh <ankesh.9323@gmail.com>
2021-10-24 23:36:45 +0530

    day-2 changes

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+#from helpinghand.home.views import Maidservices
 from home import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('Maidservices', views.Maidservices, name='Maidservices')
 ]
--- home/views.py
+++ home/views.py
@@ -3,4 +3,21 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("welcome to home page")
+    context = {
+        'variable': 'this is sent'
+    }
+    return render(request, 'index.html', context)
+   # return HttpResponse("welcome to home page")
+
+def about(request):
+    return render(request, 'about.html')
+  #  return HttpResponse("welcome to about page")
+
+def contact(request):
+    return render(request, 'contact.html')
+  #  return HttpResponse("welcome to contact page")
+
+def Maidservices(request):
+    return render(request, 'services.html')
+
+
