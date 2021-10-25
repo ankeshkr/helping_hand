2314d35f39ec5291b0d8e2b702a1be4909be56a1
vraj178 <vraj178@gmail.com>
2021-10-26 01:17:33 +0530

    added static and template folders

--- home/views.py
+++ home/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("welcome to home page")
+    context = {
+        'variable' : 'vishwa',
+        'variable_name' : 'Raj'
+    }
+    return render(request,'index.html', context)
+    # return HttpResponse("welcome to home page vishwa in home app")
+
+def about(request):
+    return HttpResponse("welcome to about page")
