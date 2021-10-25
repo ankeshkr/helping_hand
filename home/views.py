89a93c3f3eb5e29964a13f9cfe9e32c34077f50f
ankesh <ankesh.9323@gmail.com>
2021-10-25 17:51:18 +0530

    image not loading error

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
 
 # Create your views here.
 def index(request):
@@ -14,6 +16,13 @@ def about(request):
   #  return HttpResponse("welcome to about page")
 
 def contact(request):
+    if request.method == "POST":
+      name=request.POST.get('name')
+      email=request.POST.get('email')
+      phone=request.POST.get('phone')
+      desc=request.POST.get('desc')
+      contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+      contact.save()
     return render(request, 'contact.html')
   #  return HttpResponse("welcome to contact page")
 
